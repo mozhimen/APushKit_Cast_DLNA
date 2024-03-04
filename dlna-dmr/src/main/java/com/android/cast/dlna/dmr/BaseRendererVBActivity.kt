60269405db50b1d704425d10c7507472eaf3543a
Kolin Zhao & Mozhimen <mozhimen@163.com>
2024-03-05 01:00:20 +0800

    0305

---
+++ dlna-dmr/src/main/java/com/android/cast/dlna/dmr/BaseRendererVBActivity.kt
@@ -0,0 +1,48 @@
+package com.android.cast.dlna.dmr
+
+import android.os.Bundle
+import androidx.annotation.CallSuper
+import androidx.viewbinding.ViewBinding
+import com.mozhimen.basick.elemk.androidx.appcompat.commons.IActivity
+import com.mozhimen.basick.utilk.androidx.viewbinding.UtilKViewBinding
+
+/**
+ * @ClassName BaseRenderVBActivity
+ * @Description TODO
+ * @Author Mozhimen / Kolin Zhao
+ * @Date 2024/3/5 0:12
+ * @Version 1.0
+ */
+open class BaseRendererVBActivity<VB : ViewBinding> : BaseRendererActivity(), IActivity {
+
+    protected val vb: VB by lazy(mode = LazyThreadSafetyMode.NONE) {
+        UtilKViewBinding.get(this::class.java, layoutInflater/*, 0*/)
+    }
+
+    ///////////////////////////////////////////////////////////////
+
+    @CallSuper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        try {
+            initFlag()
+            initLayout()
+            initData(savedInstanceState)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////
+
+    @CallSuper
+    override fun initLayout() {
+        setContentView(vb.root)
+    }
+
+    @CallSuper
+    override fun initData(savedInstanceState: Bundle?) {
+        initView(savedInstanceState)
+        initObserver()
+    }
+}
